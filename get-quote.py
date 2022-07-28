e08c414588221a9033e2b6a3f7d4d9bd80eedf9f
mcleolee <59864232+mcleolee@users.noreply.github.com>
2022-07-28 14:56:23 +0800

    7.28+1456 #3

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,19 @@
-def print_something():
-  print("Keep it logically awesome.")
+import random
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
 
-  #print(quotes)
+def read_from_a_file():
+  print("\n")
+  print("running code.")
+
+  f = open("quotes.txt") # 变量 f 打开文件 quotes.txt
+  quotes = f.readlines()
+  f.close()
+  # last_line = len(quotes) - 1
+  last_line = 13 #
+  rnd = random.randint(0,last_line)
+
+  # print(last_line)
+  print(quotes[rnd])
 
 if __name__== "__main__":
-  print_something()
+  read_from_a_file()
